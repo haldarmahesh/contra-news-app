0cbd1f85f8449a350885c6fa4be4ab58b9080b2b
Mahesh Haldar <haldar.mahesh@gmail.com>
2020-08-24 00:24:55 +0530

    Add homescreen and directory structure

---
+++ src/presentation/screens/HomeScreen/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../component/Header";
+
+function HomeScreen(props: any) {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div>HomePage</div>
+      </div>
+    </>
+  );
+}
+
+export default HomeScreen;
