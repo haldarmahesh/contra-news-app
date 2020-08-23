63d8814a122784059ec1526ffebf4e0ff09c5aee
Mahesh Haldar <haldar.mahesh@gmail.com>
2020-08-24 00:26:34 +0530

    Add homescreen and connect

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import { connect } from "react-redux"
+import React from "react";
+import { connect } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import { getNewsList } from "./state/counter/news/news.actions";
+
 function App(props: any) {
-  console.log('>>', props);
   return (
-    <div className="App">
-    <div>Count: {props.count}</div>
-    
-    </div>
+    <>
+      <HomeScreen />
+      <div className="container">
+        <button
+          className="btn btn-primary"
+          onClick={() => props.increaseCounter()}
+        >
+          Get data
+        </button>
+      </div>
+    </>
   );
 }
 
 const mapStateToProps = (state: any) => {
   return {
-    count: state.counter.count,
-  }
-}
+    newsItems: state.newList.items,
+  };
+};
 
 const mapDispatchToProps = (dispatch: any) => {
   return {
-  }
-}
-
+    increaseCounter: () => dispatch(getNewsList),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
