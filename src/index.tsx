14bb48d14eb95146d9258727e084a758c2cbb1b7
Mahesh Haldar <haldar.mahesh@gmail.com>
2020-08-23 19:08:30 +0530

    Move store to core

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './presentation/redux/store';
+import store from './core/redux/store';
 
 
 import App from './presentation/App';
