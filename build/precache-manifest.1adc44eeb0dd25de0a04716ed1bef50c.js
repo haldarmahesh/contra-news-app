cb202b21a29be1d3785b4b6d9f4965a89c051444
Mahesh Haldar <haldar.mahesh@gmail.com>
2020-09-03 19:45:35 +0530

    Add build

---
+++ build/precache-manifest.1adc44eeb0dd25de0a04716ed1bef50c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3b2de6a42bc3586bed20afd06c8568",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bd7b70e84dece50d45c",
+    "url": "/static/css/main.ceeb835d.chunk.css"
+  },
+  {
+    "revision": "428d83e3a25d362c21b1",
+    "url": "/static/js/2.2c9fe2af.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.2c9fe2af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd7b70e84dece50d45c",
+    "url": "/static/js/main.607f52e3.chunk.js"
+  },
+  {
+    "revision": "4c15ebc6f20cd3bb8f71",
+    "url": "/static/js/runtime-main.e4e3dcfc.js"
+  }
+]);
